cccca35fa8cca628767ad13b185f496e9a5df1ec
Diegobb9517 <diegobb9517@gmail.com>
2026-06-01 19:21:51 -0600

    fix: update facebook social link in footer

--- src/App.tsx
+++ src/App.tsx
@@ -1681,7 +1681,7 @@ function App() {
                 <a href="https://instagram.com/lensique" target="_blank" rel="noopener noreferrer">
                   <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
                 </a>
-                <a href="https://facebook.com/lensique" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.facebook.com/profile.php?id=61579352515332" target="_blank" rel="noopener noreferrer">
                   <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
                 </a>
               </div>
